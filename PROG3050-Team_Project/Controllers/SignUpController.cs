c83892f4c528368824730686b78fd8b3b1ed804d
Alexander Philippopoulos <161390808+ajax815@users.noreply.github.com>
2024-09-30 17:59:51 -0400

    Removed home controller and home page

---
+++ PROG3050-Team_Project/Controllers/SignUpController.cs
@@ -0,0 +1,45 @@
+using Microsoft.AspNetCore.Mvc;
+using PROG3050_Team_Project.Models;
+using System.Diagnostics;
+using System.Text.RegularExpressions;
+
+namespace PROG3050_Team_Project.Controllers
+{
+    public class SignUpController : Controller
+    {
+        private GameVoidContext _context; 
+
+        public SignUpController(GameVoidContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public IActionResult SignUp()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult SignUp(Member member)
+        {
+            // Validate the model
+            if (ModelState.IsValid)
+            {
+                // Validate the password strength
+                var passwordCriteria = new Regex("^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[@$!%*?&])[A-Za-z\\d@$!%*?&]{8,}$");
+                if (!passwordCriteria.IsMatch(member.Password))
+                {
+                    ModelState.AddModelError("Password", "Password must be at least 8 characters long, include at least one uppercase letter, one lowercase letter, one number, and one special character.");
+                    return View(member); // Return to the signup view with validation errors
+                }
+
+                _context.Members.Add(member);
+                _context.SaveChanges();
+
+                return RedirectToAction("Index");
+            }
+
+            return View();
+        }
+    }
+}
