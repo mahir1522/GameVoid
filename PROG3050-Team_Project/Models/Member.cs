d1ee616bee0d1a4b5f5f33f2fcb9076f84126c94
Mahir Patel <104259303+mahir1522@users.noreply.github.com>
2024-10-01 19:44:36 -0400

    good to go signup page

--- PROG3050-Team_Project/Models/Member.cs
+++ PROG3050-Team_Project/Models/Member.cs
@@ -6,33 +6,34 @@ namespace PROG3050_Team_Project.Models
 {
     public class Member
     {
-        public int MemberID { get; set; }
+        public int? MemberID { get; set; }  // Nullable int
 
         [Required(ErrorMessage = "Username is required.")]
-        public string UserName { get; set; }
+        public string? UserName { get; set; }  // Nullable string 
 
         [Required(ErrorMessage = "Email is required.")]
         [EmailAddress(ErrorMessage = "Invalid email format.")]
-        public string Email {  get; set; }
+        public string? Email { get; set; }  // Nullable string 
 
         [Required(ErrorMessage = "Password is required.")]
-        public string Password { get; set; }
+        public string? Password { get; set; }  // Nullable string 
 
         public string? FullName { get; set; } = String.Empty;
 
-        public string? Gender {  get; set; } = String.Empty;
+        public string? Gender { get; set; } = String.Empty;
 
         public DateTime? BirthDate { get; set; } = DateTime.Now;
 
         public bool? WantsPromotions { get; set; } = false;
 
         public List<string>? FavoritePlatforms { get; set; } = new List<string>();
+
         public List<string>? FavoriteGameCategories { get; set; } = new List<string>();
 
         [NotMapped]
-        public Address Address { get; set; }
+        public Address? Address { get; set; }  // Nullable Address
 
-        public List<Member> FriendsAndFamily { get; set; }
+        public List<Member>? FriendsAndFamily { get; set; } = new List<Member>();  // Nullable list
 
         public Member()
         {
@@ -40,8 +41,9 @@ namespace PROG3050_Team_Project.Models
             FavoriteGameCategories = new List<string>();
             FriendsAndFamily = new List<Member>();
         }
-        public ICollection<WishList> WishLists { get; set; }
-        public ICollection<Order> Orders { get; set; }
-        public ICollection<Event> RegisteredEvents { get; set; }
+
+        public ICollection<WishList>? WishLists { get; set; }  // Nullable collections
+        public ICollection<Order>? Orders { get; set; }  // Nullable collections
+        public ICollection<Event>? RegisteredEvents { get; set; }  // Nullable collections
     }
 }
