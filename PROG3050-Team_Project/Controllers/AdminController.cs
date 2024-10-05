d63292de427eea97c69958427866e4e317f4a552
Alexander Philippopoulos <161390808+ajax815@users.noreply.github.com>
2024-10-04 20:34:42 -0400

    Added games table to admin panel

--- PROG3050-Team_Project/Controllers/AdminController.cs
+++ PROG3050-Team_Project/Controllers/AdminController.cs
@@ -1,12 +1,21 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using PROG3050_Team_Project.Models;
 
 namespace PROG3050_Team_Project.Controllers
 {
     public class AdminController : Controller
     {
+        private readonly GameVoidContext _context;
+        public AdminController(GameVoidContext context)
+        {
+            _context = context;
+        }
+        [HttpGet]
         public IActionResult Index()
         {
-            return View();
+            // Retrieving all games from Games table
+            var games = _context.Games.ToList();
+            return View(games);
         }
     }
 }
--- PROG3050-Team_Project/Program.cs
+++ PROG3050-Team_Project/Program.cs
@@ -28,6 +28,6 @@ app.UseAuthorization();
 
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=LogIn}/{action=Index}/{id?}");
+    pattern: "{controller=Admin}/{action=Index}/{id?}");
 
 app.Run();
